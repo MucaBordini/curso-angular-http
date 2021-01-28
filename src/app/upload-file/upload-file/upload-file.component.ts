87d813b553b34f66ef6050f7416a9ee2cf163cce
MucaBordini <mucabordini@gmail.com>
2021-01-28 09:45:51 -0300

    Criando operador RxJS customizado

--- src/app/upload-file/upload-file/upload-file.component.ts
+++ src/app/upload-file/upload-file/upload-file.component.ts
@@ -2,28 +2,27 @@ import { environment } from './../../../environments/environment';
 import { UploadFileService } from './../upload-file.service';
 import { Component, OnInit } from '@angular/core';
 import { HttpEvent, HttpEventType } from '@angular/common/http';
+import { filterResponse, uploadProgress } from 'src/app/shared/rxjs-operators';
 
 @Component({
   selector: 'app-upload-file',
   templateUrl: './upload-file.component.html',
-  styleUrls: ['./upload-file.component.css']
+  styleUrls: ['./upload-file.component.css'],
 })
 export class UploadFileComponent implements OnInit {
-
   files: Set<File>;
 
   progresso = 0;
 
-  constructor(private uploadService: UploadFileService) { }
+  constructor(private uploadService: UploadFileService) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   onChange(event) {
     const selectedFiles = <FileList>event.srcElement.files;
     const fileNames = [];
     this.files = new Set();
-    for(let i = 0; i < selectedFiles.length; i++){
+    for (let i = 0; i < selectedFiles.length; i++) {
       fileNames.push(selectedFiles[i].name);
       this.files.add(selectedFiles[i]);
     }
@@ -35,18 +34,17 @@ export class UploadFileComponent implements OnInit {
 
   onUpload() {
     if (this.files && this.files.size > 0) {
-      this.uploadService.upload(this.files, environment.BASE_URL + '/upload')
-        .subscribe((event: HttpEvent<Object>) => {
-          console.log(event);
-          if (event.type == HttpEventType.Response) {
-            console.log('Upload concluído')
-          } else if (event.type == HttpEventType.UploadProgress) {
-            const porcentagem = Math.round((event.loaded * 100) / event.total);
-            console.log('Progresso: ', porcentagem);
-            this.progresso = porcentagem;
-          }
-        })
+      this.uploadService
+        .upload(this.files, environment.BASE_URL + '/upload')
+        .pipe(
+          uploadProgress(progress => {
+            console.log(progress);
+            this.progresso = progress
+          }),
+          filterResponse()
+        )
+        .subscribe(response => console.log('Upload concluído'));
+
     }
   }
-
 }
